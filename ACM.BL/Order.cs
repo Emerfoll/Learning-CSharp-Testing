bdfa70dc5206a24f0b43e966f7d1ac5e959db4bf
Emerfoll <66702595+Emerfoll@users.noreply.github.com>
2022-05-10 12:17:24 -0500

    Adding class for orders.

--- ACM.BL/Order.cs
+++ ACM.BL/Order.cs
@@ -6,5 +6,50 @@ namespace ACM.BL
         public Order()
         {
         }
+        public Order(int orderId)
+        {
+            OrderId = orderId;
+        }
+        
+        public DateTimeOffset? OrderDate { get; set; }
+        public int OrderId { get; set; }
+
+        /// <summary>
+        /// Retrieve one order.
+        /// </summary>
+        public Order Retrieve(int orderId)
+        {
+            // retrieve order code
+
+            return new Order();
+        }
+
+        /// <summary>
+        /// Saves the current order
+        /// </summary>
+        /// <returns></returns>
+        public bool Save()
+        {
+            // code that saves defined order
+
+            return true;
+        }
+
+        /// <summary>
+        /// Validates the order data.
+        /// </summary>
+        /// <returns></returns>
+        public bool Validate()
+        {
+            var isValid = true;
+
+            if (OrderDate == null) isValid = false;
+
+            return isValid;
+        }
+
+
+
+
     }
 }
