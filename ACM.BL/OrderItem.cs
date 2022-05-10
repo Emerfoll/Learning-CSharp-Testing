e62083c8430e646938b4bf71493f5860733662d2
Emerfoll <66702595+Emerfoll@users.noreply.github.com>
2022-05-10 12:23:20 -0500

    Adding class for order item.

---
+++ ACM.BL/OrderItem.cs
@@ -0,0 +1,56 @@
+﻿using System;
+namespace ACM.BL
+{
+    public class OrderItem
+    {
+        public OrderItem()
+        {
+        }
+        public OrderItem(int orderItemId)
+        {
+            OrderItemId = orderItemId;
+        }
+
+        public int OrderItemId { get; set; }
+        public int ProductId { get; set; }
+        public decimal? PurchasedPrice { get; set; }
+        public int Quantity { get; set; }
+
+        /// <summary>
+        /// Retrieve order item.
+        /// </summary>
+        public OrderItem Retrieve(int orderItemId)
+        {
+            // retrieve ordered item code
+
+            return new OrderItem();
+        }
+
+        /// <summary>
+        /// Saves the current order item.
+        /// </summary>
+        /// <returns></returns>
+        public bool Save()
+        {
+            // code that saves defined order item
+
+            return true;
+        }
+
+        /// <summary>
+        /// Validates the product data.
+        /// </summary>
+        /// <returns></returns>
+        public bool Validate()
+        {
+            var isValid = true;
+
+            if (Quantity <= 0) isValid = false;
+            if (ProductId <= 0) isValid = false;
+            if (PurchasedPrice == null) isValid = false;
+
+            return isValid;
+        }
+
+    }
+}
