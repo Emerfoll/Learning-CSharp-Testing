ece8b6b0db2738370147f1e55c7f797bbf4eddd5
Emerfoll <66702595+Emerfoll@users.noreply.github.com>
2022-05-11 14:55:30 -0500

    Address class added

--- ACM.BL/Address.cs
+++ ACM.BL/Address.cs
@@ -14,13 +14,31 @@ namespace ACM.BL
         public int AddressId { get; set; }
         public int AddressType { get; set; }
         public string City { get; set; }
-        public string County { get; set; }
+        public string Country { get; set; }
         public string PostalCode { get; set; }
         public string State { get; set; }
         public string StreetLine1 { get; set; }
         public string StreetLine2 { get; set; }
 
+        /// <summary>
+        /// Validates the address data
+        /// </summary>
+        /// <returns></returns>
+        
+        public bool Validate()
+        {
+            var isValid = true;
 
+            if (string.IsNullOrWhiteSpace(PostalCode)) isValid = false;
+
+
+            //if (string.IsNullOrWhiteSpace(City)) isValid = false;
+            //if (string.IsNullOrWhiteSpace(Country)) isValid = false;
+            //if (string.IsNullOrWhiteSpace(State)) isValid = false;
+            //if (string.IsNullOrWhiteSpace(StreetLine1)) isValid = false;
+
+            return isValid;
+        }
 
 
 
