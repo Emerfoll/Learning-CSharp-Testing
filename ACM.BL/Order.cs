cdfc92c0ed2338d3ee7175b4edd235933f9f367c
Emerfoll <66702595+Emerfoll@users.noreply.github.com>
2022-05-11 22:05:03 -0500

    added test for order repo

--- ACM.BL/Order.cs
+++ ACM.BL/Order.cs
@@ -14,27 +14,7 @@ namespace ACM.BL
         public DateTimeOffset? OrderDate { get; set; }
         public int OrderId { get; set; }
 
-        /// <summary>
-        /// Retrieve one order.
-        /// </summary>
-        public Order Retrieve(int orderId)
-        {
-            // retrieve order code
-
-            return new Order();
-        }
-
-        /// <summary>
-        /// Saves the current order
-        /// </summary>
-        /// <returns></returns>
-        public bool Save()
-        {
-            // code that saves defined order
-
-            return true;
-        }
-
+        
         /// <summary>
         /// Validates the order data.
         /// </summary>
@@ -47,8 +27,6 @@ namespace ACM.BL
 
             return isValid;
         }
-
-
 
 
     }
