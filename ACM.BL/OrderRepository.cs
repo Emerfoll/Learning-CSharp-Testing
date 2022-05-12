cb0aae9eb42c02a8758d85efebcbebcef2216558
Emerfoll <66702595+Emerfoll@users.noreply.github.com>
2022-05-11 21:56:20 -0500

    added order repo class

---
+++ ACM.BL/OrderRepository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace ACM.BL
+{
+    public class OrderRepository
+    {
+        /// <summary>
+        /// Retrieve one order
+        /// </summary>
+        public Order Retrieve(int orderId)
+        {
+            // Create instance of the order class
+            // pass requested ID
+            Order order = new Order(orderId);
+
+            // code to retrieve defined order
+
+            // temp hard coded values to return
+
+            if (orderId == 10)
+            {
+                // use current year in hard coded data
+                order.OrderDate = new DateTimeOffset(DateTime.Now.Year, 4, 14, 10, 00, 00, new TimeSpan(7, 0, 0));
+            }
+            return order;
+
+        }
+
+        /// <summary>
+        /// Saves current order
+        /// </summary>
+        /// <returns></returns>
+        public bool Save(Order order)
+        {
+            // code to save order
+
+            return true;
+        }
+    }
+}
